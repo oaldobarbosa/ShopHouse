1e079b0d3dd3d0b7a9e976b3be5cae4e19a070b1
Aldo Da Silva Barbosa <aldobarbosaads@gmail.com>
2021-06-15 22:59:38 -0300

    telas view e edit imovel

---
+++ app/src/main/java/com/example/shophouse/FormEditarImovel.java
@@ -0,0 +1,14 @@
+package com.example.shophouse;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FormEditarImovel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form_editar_imovel);
+    }
+}
